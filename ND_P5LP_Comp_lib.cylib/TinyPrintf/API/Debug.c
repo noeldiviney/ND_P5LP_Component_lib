fe88eb5dff966f4f07b1200b8778a8bd86ba00fd
noeldiviney <noel@ayrenet.com>
2017-12-12 11:01:04 +1100

    updating

---
+++ ND_P5LP_Comp_lib.cylib/TinyPrintf/API/Debug.c
@@ -0,0 +1,47 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include "`$INSTANCE_NAME`_API.h"
+#include "`$INSTANCE_NAME`_Example.h"
+#include "`$INSTANTE_NAME`_Debug.h"
+
+void printMenu
+void printMenu(void)
+{
+    // Print Menu.
+    sprintf(transmitBuffer, "*****************\r\n");
+    // Send out the data.
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  Menu:\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  M - Print menu\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  0 - Reset counter\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  1 - Increment counter\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  2 - Start continues counter\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  3 - Stop continues counter\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  4 - Get switch and LED state\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  5 - Set LED on\r\n");
+    UART_1_PutString(transmitBuffer);
+    sprintf(transmitBuffer, "*  6 - Set LED off\r\n");
+    UART_1_PutString(transmitBuffer);
+}
+
+void scopeTrig(uint8 val)
+{
+	SCOPE_TRIG(val);
+}
---
+++ ND_P5LP_Comp_lib.cylib/TinyPrintf/API/Debug.h
@@ -0,0 +1,37 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#if !defined(DEBUG_H)
+#define DEBUG_H
+
+#define FALSE				0
+#define TRUE 				1
+#define OFF					0
+#define ON					1
+#define XMIT_BFR_SIZE		256
+
+/* Variable to store UART Received Char */
+uint9						ch;
+ /* Transmit Buffer */
+ char transmitBuffer[XMIT_BFR_SIZE];
+ uint8		outputCounter8  =  0;
+ uint16		outputCounter16 =  0;
+ uint32		outputCounter32 =  0;
+ uint8		flagPrint		=  FALSE;
+ uint8		flagContCntr	=  FALSE;
+ 
+ const char* strOn			=  "ON";
+ const char* strOff			=  "OFF";
+ 
+ void printMenu();
+ #endif		/* DEBUG_H */
+ 
+ /* [] END OF FILE */
