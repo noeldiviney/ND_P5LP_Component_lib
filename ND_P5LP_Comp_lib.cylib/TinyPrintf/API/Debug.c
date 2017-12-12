2fb81435766fa3b7d12c6101164d5a3ac46c501b
noeldiviney <noel@ayrenet.com>
2017-12-12 12:04:48 +1100

    updating

--- ND_P5LP_Comp_lib.cylib/TinyPrintf/API/Debug.c
+++ ND_P5LP_Comp_lib.cylib/TinyPrintf/API/Debug.c
@@ -17,15 +17,34 @@
 void printMenu(void)
 {
     // Print Menu.
+	UART_dbg_PutString("**********************);
+    UART_dbg_PutString("*  Menu:");
+	NEWLINE();
+    UART_dbg_PutString("*  M - Print menu");
+	NEWLINE();
+    UART_dbg_PutString("*  0 - Reset counter");
+	NEWLINE();
+    UART_dbg_PutString("*  1 - Increment counter");
+	NEWLINE();
+    UART_dbg_PutString("*  3 - Stop continues counter");
+	NEWLINE();
+    UART_dbg_PutString("*  4 - Get switch and LED state");
+	NEWLINE();
+    UART_dbg_PutString("*  5 - Set LED on");
+	NEWLINE();
+    UART_dbg_PutString("*  6 - Set LED off");
+	NEWLINE();
+	
+	#if 0
     sprintf(transmitBuffer, "*****************\r\n");
     // Send out the data.
-    UART_1_PutString(transmitBuffer);
+    UART_dbg_PutString(transmitBuffer);
     sprintf(transmitBuffer, "*  Menu:\r\n");
-    UART_1_PutString(transmitBuffer);
+    UART_dbg_PutString(transmitBuffer);
     sprintf(transmitBuffer, "*  M - Print menu\r\n");
-    UART_1_PutString(transmitBuffer);
+    UART_dbg_PutString(transmitBuffer);
     sprintf(transmitBuffer, "*  0 - Reset counter\r\n");
-    UART_1_PutString(transmitBuffer);
+    UART_dbg_PutString(transmitBuffer);
     sprintf(transmitBuffer, "*  1 - Increment counter\r\n");
     UART_1_PutString(transmitBuffer);
     sprintf(transmitBuffer, "*  2 - Start continues counter\r\n");
@@ -38,6 +57,7 @@ void printMenu(void)
     UART_1_PutString(transmitBuffer);
     sprintf(transmitBuffer, "*  6 - Set LED off\r\n");
     UART_1_PutString(transmitBuffer);
+#endif
 }
 
 void scopeTrig(uint8 val)
