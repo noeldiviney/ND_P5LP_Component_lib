527f1d90559691fd5cad2b288653d031a670ab07
noeldiviney <noel@ayrenet.com>
2017-12-15 14:51:26 +1100

    updating

--- ND_P5LP_Comp_lib.cylib/P5LP_059_Debug/API/P5LP_059_Debug.c
+++ ND_P5LP_Comp_lib.cylib/P5LP_059_Debug/API/P5LP_059_Debug.c
@@ -9,20 +9,26 @@
  *
  * ========================================
 */
+#include "project.h"
+
+
 /*
- *     Initialization example code to be copied into "main.c"
- *
- *   UART_dbg_Start();
- *
- *   I2C_M_FF_Start();
- *   PCF8574_LCD_Start();
- *    
- *   TinyPrintf_init();
- *   
- *   printf("%s", "Hello World");
- *   NewLine();
- *   TriggerScope();
- *   PCF8574_LCD_Test();
+ *     Initialization code to be called from "main.c"
  *
  */
+void `$INSTANCE_NAME`_Start(void)
+{
+    UART_dbg_Start();
+
+   I2C_M_FF_Start();
+   PCF8574_LCD_Start();
+    
+   TinyPrintf_init();
+   
+   printf("%s", "Hello World");
+   NewLine();
+   TriggerScope();
+   PCF8574_LCD_Test();
+}
+
 /* [] END OF FILE */
